fbb2cc33ec0356a9e9e0c4400f7471ee6aea3d67
Jhairo Rivera <jhairorivera@Jhairos-MacBook-Pro.local>
2020-06-05 18:07:30 -0500

    Cambios

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use('/', routes());
 
 //Validacion host y puerto cuando se trabaja en entornos de desarrollo y produccion
 const host = process.env.host || '0.0.0.0';
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 app.listen(port, host, ()=>{
     console.log('El servidor esta funcionando');    
 });
